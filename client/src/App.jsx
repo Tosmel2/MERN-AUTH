dee43c67c978a02bd772868c6c88a060126eb210
tosmel2 <adewaletosin0808@gmail.com>
2023-08-24 08:32:25 -0700

    feat: added header components through all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import About from './pages/About'
 import SignIn from './pages/Signin'
 import SignUp from './pages/Signup'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
